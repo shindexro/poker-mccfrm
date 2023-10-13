b35bae0918d0b71f7f02ae9c852d7df060538806
shindexro <shindexro@gmail.com>
2023-10-14 00:49:37 +0100

    implement structure of EHS lookup table

---
+++ src/tables/include/tables/ehs_table.h
@@ -0,0 +1,36 @@
+#ifndef __CLASS_EHS_TABLE_H__
+#define __CLASS_EHS_TABLE_H__
+
+#include <string>
+#include <string.h>
+#include <iostream>
+
+#define NUM_ISOMORPHIC_FLOP 1286792
+#define NUM_ISOMORPHIC_TURN 13960050
+
+using namespace std;
+
+class EHSTable
+{
+public:
+    int tableFlop[3][3];
+    int tableTurn[3][3];
+    static float EHSFlop[NUM_ISOMORPHIC_FLOP];
+    static float EHSTurn[NUM_ISOMORPHIC_TURN];
+    static float histogramsFlop[NUM_ISOMORPHIC_FLOP];
+
+    EHSTable();
+
+private:
+    static string EHSTable5Cards;
+    static string EHSTable6Cards;
+
+    void ClusterFlop();
+    void Generate5CardsTable();
+    void Generate6CardsTable();
+    void CalculateFlopHistograms();
+    static void SaveToFile();
+    static void LoadFromFile();
+};
+
+#endif
---
+++ src/tables/src/ehs_table.cpp
@@ -0,0 +1,45 @@
+#include "tables/ehs_table.h"
+
+string EHSTable::EHSTable5Cards = "EHSTable5Cards.txt";
+string EHSTable::EHSTable6Cards = "EHSTable6Cards.txt";
+
+EHSTable::EHSTable() : tableFlop{},
+                       tableTurn{}
+{
+    // In essence, go through the 2+3 HandIndexer set and combine with opponent, turn, river
+    // 1286792 * 1081*990 = 1 377 111 930 480 combinations
+
+    LoadFromFile();
+
+    if (EHSFlop == NULL)
+    {
+        Generate5CardsTable();
+        SaveToFile();
+        Generate6CardsTable();
+        SaveToFile();
+    }
+    else if (EHSTurn == NULL)
+    {
+        Generate6CardsTable();
+        SaveToFile();
+    }
+    CalculateFlopHistograms();
+    ClusterFlop();
+}
+
+void EHSTable::ClusterFlop()
+{
+}
+
+void EHSTable::Generate5CardsTable()
+{
+    cout << "Calculating effective hand strength table for 2 + 3 (1286792)" << endl;
+
+    int cards[5] = {};
+    long deadCardMask = 0;
+
+    for (int i = 0; i < NUM_ISOMORPHIC_FLOP; i++)
+    {
+        memset(tableFlop, 0, sizeof(tableFlop));
+    }
+}
