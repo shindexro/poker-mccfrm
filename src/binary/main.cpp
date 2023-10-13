32bcb228b3ab7e8a11198f169456888b4a14c266
shindexro <shindexro@gmail.com>
2023-10-13 21:02:24 +0100

    add trivial tests

--- src/binary/main.cpp
+++ src/binary/main.cpp
@@ -1,22 +1,28 @@
 #include "game/card.h"
+#include "game/deck.h"
 #include <iostream>
 #include <string>
 
 class Program
 {
     public:
-    static void Index()
+    static void Test()
     {
-        cout << "Creating 2 card index..." << endl;
+        cout << "Testing card creation" << endl;
         Card card = Card("Ts");
         cout << card.ToString() << endl;
+
+        cout << "Testing deck creation" << endl;
+        Deck deck = Deck(0);
+        deck.Draw_(3);
+        cout << deck.NumRemainingCards() << endl;
     }
 };
 
 int main(int argc, char *argv[])
 {
     Program program;
-    program.Index();
+    program.Test();
     
     return 0;
 }
