299fd54b171886b5fb32ad224288cdb6f4731108
shindexro <shindexro@gmail.com>
2023-11-07 18:49:20 +0000

    add hand creation tests

---
+++ test/hand.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "game/hand.h"
+
+using namespace testing;
+
+TEST(HandTest, CreateHandWithCardBitmap)
+{
+    ulong bitmap = 0ul;
+    auto cards = vector<Card>({Card("As"), Card("Qh"), Card("2d")});
+    for (auto card : cards)
+    {
+        bitmap |= card.Bitmask();
+    }
+
+    Hand hand(bitmap);
+    EXPECT_THAT(hand.cards, UnorderedElementsAreArray(cards));
+}
+
+TEST(HandTest, CreateHandWithCardStrings)
+{
+    auto cards = vector<Card>({Card("As"), Card("Qh"), Card("2d")});
+    auto cardString = vector<string>({"As", "Qh", "2d"});
+
+    Hand hand(cardString);
+    EXPECT_THAT(hand.cards, UnorderedElementsAreArray(cards));
+}
