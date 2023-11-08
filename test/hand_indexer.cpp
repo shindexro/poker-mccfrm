8621b06b318302484819fea20eb3087ed62793eb
shindexro <shindexro@gmail.com>
2023-11-08 15:16:06 +0000

    add hand indexer test

---
+++ test/hand_indexer.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <random>
+
+#include "tables/hand_indexer.h"
+
+using namespace testing;
+
+class HandIndexerTest : public Test
+{
+protected:
+    void SetUp() override
+    {
+        if (!handIndexerInitialised)
+        {
+            handIndexer.Initialise();
+            handIndexerInitialised = true;
+        }
+
+        vector<int> cardsPerRound{1, 2};
+        handIndexer.Construct(cardsPerRound);
+    }
+
+    static bool handIndexerInitialised;
+    HandIndexer handIndexer;
+};
+
+bool HandIndexerTest::handIndexerInitialised = false;
+
+TEST_F(HandIndexerTest, IsomorphicHandsHaveSameIndex)
+{
+    vector<int> cards1{1, 2};
+    vector<int> cards2{2, 3};
+    int handIndex1 = handIndexer.IndexLastRound(cards1);
+    int handIndex2 = handIndexer.IndexLastRound(cards2);
+
+    EXPECT_EQ(handIndex1, handIndex2);
+}
