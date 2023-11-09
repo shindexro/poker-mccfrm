456f872fb14cbf89d1c5703aadce3be5a9ccde4f
shindexro <shindexro@gmail.com>
2023-11-09 20:17:24 +0000

    make global variables closer to production

--- src/binary/main.cpp
+++ src/binary/main.cpp
@@ -80,11 +80,11 @@ private:
     {
         cout << "Starting Monte Carlo Counterfactual Regret Minimization (MCCFRM)..." << endl;
 
-        long StrategyInterval = max(1, 1000 / Global::NOF_THREADS); // bb rounds before updating player strategy (recursive through tree) 10k
+        long StrategyInterval = max(1, 10000 / Global::NOF_THREADS); // bb rounds before updating player strategy (recursive through tree) 10k
         long PruneThreshold = 20000000 / Global::NOF_THREADS;       // bb rounds after this time we stop checking all actions, 200 minutes
         long LCFRThreshold = 20000000 / Global::NOF_THREADS;        // bb rounds when to stop discounting old regrets, no clue what it should be
         long DiscountInterval = 1000000 / Global::NOF_THREADS;      // bb rounds, discount values periodically but not every round, 10 minutes
-        long SaveToDiskInterval = 1000000 / Global::NOF_THREADS;
+        long SaveToDiskInterval = 100000 / Global::NOF_THREADS;
         long testGamesInterval = 100000 / Global::NOF_THREADS;
 
         atomic<long> sharedLoopCounter = 0;
