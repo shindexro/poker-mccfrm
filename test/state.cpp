be5af478d8685a0c8340a34108c690f34d9fa22b
shindexro <shindexro@gmail.com>
2023-11-11 17:51:13 +0000

    add tests

--- test/state.cpp
+++ test/state.cpp
@@ -87,3 +87,40 @@ TEST_F(StateTest, ChanceStateDealOneCardOnRiver)
 
     ASSERT_EQ(nextState->community.cards.size(), state.community.cards.size() + 1);
 }
+
+TEST_F(StateTest, ChanceStateChildIsOneBettingRoundAfter)
+{
+    ChanceState state = ChanceState();
+    state.CreateChildren();
+    auto nextState = state.children[0];
+
+    ASSERT_EQ(nextState->community.bettingRound, state.community.bettingRound + 1);
+}
+
+TEST_F(StateTest, ChanceStateChildHasBBMinRaise)
+{
+    ChanceState state = ChanceState();
+    state.CreateChildren();
+    auto nextState = state.children[0];
+
+    ASSERT_EQ(nextState->community.minRaise, Global::BB);
+}
+
+TEST_F(StateTest, ChanceStateChildIsBettingRound)
+{
+    ChanceState state = ChanceState();
+    state.CreateChildren();
+    auto nextState = state.children[0];
+
+    ASSERT_EQ(nextState->community.bettingRound, true);
+}
+
+TEST_F(StateTest, ChanceStateChildPlayerToPlayUnchanged)
+{
+    ChanceState state = ChanceState();
+    state.CreateChildren();
+    auto nextState = state.children[0];
+
+    ASSERT_EQ(nextState->community.lastPlayer, state.community.lastPlayer);
+    ASSERT_EQ(nextState->community.playerToMove, state.community.playerToMove);
+}
