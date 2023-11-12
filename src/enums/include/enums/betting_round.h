3e28b43d3e6791618fda08af00e55934ca4c444c
shindexro <shindexro@gmail.com>
2023-11-12 17:33:15 +0000

    move into namespace

--- src/enums/include/enums/betting_round.h
+++ src/enums/include/enums/betting_round.h
@@ -3,37 +3,39 @@
 
 #include <string>
 
-enum BettingRound
+namespace poker
 {
-    Preflop = 0,
-    Flop,
-    Turn,
-    River
-};
-
-std::ostream &operator<<(std::ostream &out, const BettingRound &value)
-{
-    std::string s = [value]
+    enum BettingRound
     {
+        Preflop = 0,
+        Flop,
+        Turn,
+        River
+    };
+
+    std::ostream &operator<<(std::ostream &out, const BettingRound &value)
+    {
+        std::string s = [value]
+        {
 #define PROCESS_VAL(p) \
     case (p):          \
         return #p;
-        switch (value)
-        {
-            PROCESS_VAL(Preflop);
-            PROCESS_VAL(Flop);
-            PROCESS_VAL(Turn);
-            PROCESS_VAL(River);
-        }
-        return "N/A";
+            switch (value)
+            {
+                PROCESS_VAL(Preflop);
+                PROCESS_VAL(Flop);
+                PROCESS_VAL(Turn);
+                PROCESS_VAL(River);
+            }
+            return "N/A";
 #undef PROCESS_VAL
-    }();
-    return out << s;
-}
+        }();
+        return out << s;
+    }
 
-BettingRound &operator++(BettingRound &round)
-{
-    return round = static_cast<BettingRound>(static_cast<int>(round) + 1);
-}
-
+    BettingRound &operator++(BettingRound &round)
+    {
+        return round = static_cast<BettingRound>(static_cast<int>(round) + 1);
+    }
+} // namespace poker
 #endif
