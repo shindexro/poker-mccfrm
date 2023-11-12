d9ba5e792d1462411ca2c7b0306c2684c724eebb
shindexro <shindexro@gmail.com>
2023-11-12 16:29:56 +0000

    improve enum printing

--- src/enums/include/enums/hand_ranking.h
+++ src/enums/include/enums/hand_ranking.h
@@ -10,3 +10,26 @@ enum HandRanking
     FourOfAKind,
     StraightFlush,
 };
+
+std::ostream &operator<<(std::ostream &out, const HandRanking &value)
+{
+    return out << [value]
+    {
+#define PROCESS_VAL(p) \
+    case (p):          \
+        return #p;
+        switch (value)
+        {
+            PROCESS_VAL(HighCard);
+            PROCESS_VAL(HandRanking::Pair);
+            PROCESS_VAL(TwoPair);
+            PROCESS_VAL(ThreeOfAKind);
+            PROCESS_VAL(Straight);
+            PROCESS_VAL(Flush);
+            PROCESS_VAL(FullHouse);
+            PROCESS_VAL(FourOfAKind);
+            PROCESS_VAL(StraightFlush);
+        }
+#undef PROCESS_VAL
+    }();
+}
