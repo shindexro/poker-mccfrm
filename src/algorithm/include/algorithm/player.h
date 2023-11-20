5ed343a865487e3322034a6d73d83581163a14ec
shindexro <shindexro@gmail.com>
2023-11-20 22:11:59 +0000

    comment out unused variable

--- src/algorithm/include/algorithm/player.h
+++ src/algorithm/include/algorithm/player.h
@@ -15,7 +15,7 @@ namespace poker
         Player(int id);
 
         // the whole state is passed for simlicity but a player should not access other player's hand
-        virtual Action NextAction(PlayState &state) { throw invalid_argument("Not implemented"); };
+        virtual Action NextAction(PlayState &/*state*/) { throw invalid_argument("Not implemented"); };
     };
 } // namespace poker
 
