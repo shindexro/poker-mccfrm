f6dc67f1b11d3c838522786e37f382f8d88d7ca8
shindexro <shindexro@gmail.com>
2023-11-21 01:32:34 +0000

    improve pretty print format

--- src/algorithm/src/game.cpp
+++ src/algorithm/src/game.cpp
@@ -11,7 +11,8 @@ namespace poker
     {
         while (!(dynamic_cast<TerminalState *>(state.get())))
         {
-            std::cout << *state << std::endl;
+            state->PrettyPrint(std::cout);
+            std::cout << std::endl;
             if (dynamic_cast<ChanceState *>(state.get()))
             {
                 state = state->DoRandomAction();
