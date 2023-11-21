af1ddc0a22a3b6b9f439001fca5007c2d16f24ef
shindexro <shindexro@gmail.com>
2023-11-21 02:31:14 +0000

    move include to header

---
+++ src/binary/include/binary/main.h
@@ -0,0 +1,23 @@
+#ifndef __CLASS_MAIN_H__
+#define __CLASS_MAIN_H__
+
+#include "game/card.h"
+#include "game/deck.h"
+#include "abstraction/global.h"
+#include "abstraction/infoset.h"
+#include "abstraction/chance_state.h"
+#include "tables/hand_indexer.h"
+#include "tables/ochs_table.h"
+#include "tables/emd_table.h"
+#include "utils/utils.h"
+#include "utils/random.h"
+#include "algorithm/trainer.h"
+#include "abstraction/state.h"
+#include "algorithm/game.h"
+#include "algorithm/ai_player.h"
+#include "algorithm/interactive_player.h"
+
+#include <iostream>
+#include <string>
+
+#endif
--- src/binary/main.cpp
+++ src/binary/main.cpp
@@ -1,21 +1,4 @@
-#include "game/card.h"
-#include "game/deck.h"
-#include "abstraction/global.h"
-#include "abstraction/infoset.h"
-#include "abstraction/chance_state.h"
-#include "tables/hand_indexer.h"
-#include "tables/ochs_table.h"
-#include "tables/emd_table.h"
-#include "utils/utils.h"
-#include "utils/random.h"
-#include "algorithm/trainer.h"
-#include "abstraction/state.h"
-#include "algorithm/game.h"
-#include "algorithm/ai_player.h"
-#include "algorithm/interactive_player.h"
-
-#include <iostream>
-#include <string>
+#include "binary/main.h"
 
 namespace poker
 {
@@ -220,8 +203,6 @@ namespace poker
 
 int main()
 {
-    Global global = Global();
-
     poker::Program program;
     program.StartGame();
 
