e09605dbdb187c6502fd46959169b102532774ad
shindexro <shindexro@gmail.com>
2023-11-24 01:04:11 +0000

    try to fix syntax

--- test/evaluator.cpp
+++ test/evaluator.cpp
@@ -14,29 +14,29 @@ protected:
         if (initialised)
             return;
 
+        evaluator = poker::Evaluator();
         evaluator.Initialise();
         initialised = true;
     }
 
     static bool initialised;
-    static poker::Evaluator evaluator;
+    poker::Evaluator evaluator;
 };
 
 bool EvaluatorTest::initialised = false;
-poker::Evaluator evaluator = poker::Evaluator();
 
 TEST_F(EvaluatorTest, SameHandsHaveSameEvaluation)
 {
-    auto eval1 = EvaluatorTest::evaluator.Evaluate(0b1111100);
-    auto eval2 = EvaluatorTest::evaluator.Evaluate(0b1111100);
+    auto eval1 = evaluator.Evaluate(0b1111100);
+    auto eval2 = evaluator.Evaluate(0b1111100);
 
     EXPECT_EQ(eval1, eval2);
 }
 
 TEST_F(EvaluatorTest, HandsWithSameStrengthHaveSameEvaluation)
 {
-    auto eval1 = EvaluatorTest::evaluator.Evaluate(214098);
-    auto eval2 = EvaluatorTest::evaluator.Evaluate(247874);
+    auto eval1 = evaluator.Evaluate(214098);
+    auto eval2 = evaluator.Evaluate(247874);
 
     EXPECT_EQ(eval1, eval2);
 }
