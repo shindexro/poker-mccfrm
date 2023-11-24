b9c0d1b5c24c0d409ffea316ad3e2c4dbb2c8843
shindexro <shindexro@gmail.com>
2023-11-24 00:33:50 +0000

    add evaluator tests

---
+++ test/evaluator.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "tables/evaluator.h"
+
+using namespace testing;
+using namespace poker;
+
+class EvaluatorTest : public Test
+{
+protected:
+    void SetUp() override
+    {
+        if (initialised)
+            return;
+
+        evaluator = poker::Evaluator();
+        evaluator.Initialise();
+        initialised = true;
+    }
+
+    static bool initialised;
+    static poker::Evaluator evaluator;
+};
+
+TEST_F(EvaluatorTest, SameHandsHaveSameEvaluation)
+{
+    auto eval1 = evaluator.Evaluate(0b1111100);
+    auto eval2 = evaluator.Evaluate(0b1111100);
+
+    EXPECT_EQ(eval1, eval2);
+}
+
+TEST_F(EvaluatorTest, HandsWithSameStrengthHaveSameEvaluation)
+{
+    auto eval1 = evaluator.Evaluate(214098);
+    auto eval2 = evaluator.Evaluate(247874);
+
+    EXPECT_EQ(eval1, eval2);
+}
