5562941d1b944f97540f5ab2180a6b5ed36fc8bb
shindexro <shindexro@gmail.com>
2023-11-24 01:02:42 +0000

    try to fix syntax

--- test/evaluator.cpp
+++ test/evaluator.cpp
@@ -27,16 +27,16 @@ poker::Evaluator evaluator = poker::Evaluator();
 
 TEST_F(EvaluatorTest, SameHandsHaveSameEvaluation)
 {
-    auto eval1 = evaluator.Evaluate(0b1111100);
-    auto eval2 = evaluator.Evaluate(0b1111100);
+    auto eval1 = EvaluatorTest::evaluator.Evaluate(0b1111100);
+    auto eval2 = EvaluatorTest::evaluator.Evaluate(0b1111100);
 
     EXPECT_EQ(eval1, eval2);
 }
 
 TEST_F(EvaluatorTest, HandsWithSameStrengthHaveSameEvaluation)
 {
-    auto eval1 = evaluator.Evaluate(214098);
-    auto eval2 = evaluator.Evaluate(247874);
+    auto eval1 = EvaluatorTest::evaluator.Evaluate(214098);
+    auto eval2 = EvaluatorTest::evaluator.Evaluate(247874);
 
     EXPECT_EQ(eval1, eval2);
 }
