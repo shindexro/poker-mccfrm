66f1ac33b99f0b169dd8d4bec9b8fc2db702a179
shindexro <shindexro@gmail.com>
2023-11-25 23:27:54 +0000

    expose mccfrm traverse method

--- src/algorithm/include/algorithm/trainer.h
+++ src/algorithm/include/algorithm/trainer.h
@@ -28,6 +28,7 @@ namespace poker
         void UpdateStrategy(shared_ptr<State> gs, int traverser);
         void UpdateStrategy(int traverser);
         int TraverseMCCFR(int traverser, bool pruned);
+        int TraverseMCCFR(shared_ptr<State> gs, int traverser, bool pruned);
         void PlayOneGame();
 
         void DiscountInfosets(float d);
@@ -39,9 +40,7 @@ namespace poker
 
     private:
         int threadIndex = 0;
-
-        int TraverseMCCFR(shared_ptr<State> gs, int traverser, bool pruned);
     };
-} //namespace poker
+} // namespace poker
 
 #endif
