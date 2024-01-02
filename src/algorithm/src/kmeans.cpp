5b1cc96297ff0dc5db6d7569803e5c38c1b69ecf
shindexro <shindexro@gmail.com>
2024-01-02 14:38:56 +0000

    Increase number of center candidates

--- src/algorithm/src/kmeans.cpp
+++ src/algorithm/src/kmeans.cpp
@@ -169,7 +169,7 @@ namespace poker
         std::cout << "K-means++ finding good starting centers..." << std::endl;
 
         // first get some samples of all data to speed up the algorithm
-        static const int centerCandidateMultiplier = 100;
+        static const int centerCandidateMultiplier = 10000;
         int maxSamples = min({k * centerCandidateMultiplier, (int)data.size()});
         auto centerCandidates = GetRandomSubset(data, maxSamples);
 
