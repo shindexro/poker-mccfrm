854cf133a1f62356c06b740713fc863bf722fc70
shindexro <shindexro@gmail.com>
2024-01-04 18:25:48 +0000

    Adjust allowed raise ratios

--- src/abstraction/src/global.cpp
+++ src/abstraction/src/global.cpp
@@ -6,35 +6,35 @@ namespace poker
     const map<BettingRound, vector<vector<float>>> Global::raiseRatiosByRoundByPlayerCount{
             {BettingRound::Preflop, {
                                         {}, // 0 players to act
-                                        {1.0f, 2.0f}, // 1 players to act
-                                        {1.0f, 2.0f}, // 2 players to act
-                                        {1.0f, 2.0f}, // 3 players to act
-                                        {1.0f, 2.0f}, // 4 players to act
-                                        {1.0f, 2.0f}, // 5 players to act
-                                        {1.0f, 2.0f}, // 6 players to act
+                                        {2.0f}, // 1 players to act
+                                        {2.0f}, // 2 players to act
+                                        {2.0f}, // 3 players to act
+                                        {2.0f}, // 4 players to act
+                                        {2.0f}, // 5 players to act
+                                        {2.0f}, // 6 players to act
                                     }},
             {BettingRound::Flop, {
                                         {},
-                                        {0.1f, 0.25f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 1 players to act
-                                        {0.1f, 0.25f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 2 players to act
-                                        {0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 3 players to act
+                                        {0.2f, 0.33f, 0.67f, 1.0f, 1.5f}, // 1 players to act
+                                        {0.2f, 0.33f, 0.67f, 1.0f, 1.5f}, // 2 players to act
+                                        {0.5f, 1.0f}, // 3 players to act
                                         {0.5f, 1.0f}, // 4 players to act
                                         {0.5f, 1.0f}, // 5 players to act
                                         {0.5f, 1.0f}, // 6 players to act
                                     }},
             {BettingRound::Turn, {
                                         {},
-                                        {0.1f, 0.25f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 1 players to act
-                                        {0.1f, 0.25f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 2 players to act
-                                        {0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 3 players to act
+                                        {0.2f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 1 players to act
+                                        {0.2f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 2 players to act
+                                        {0.5f, 1.0f}, // 3 players to act
                                         {0.5f, 1.0f}, // 4 players to act
                                         {0.5f, 1.0f}, // 5 players to act
                                         {0.5f, 1.0f}, // 6 players to act
                                     }},
             {BettingRound::River, {
                                         {},
-                                        {0.1f, 0.25f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 1 players to act
-                                        {0.1f, 0.25f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 2 players to act
+                                        {0.1f, 0.22f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 1 players to act
+                                        {0.1f, 0.22f, 0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 2 players to act
                                         {0.33f, 0.67f, 1.0f, 1.5f, 2.0f}, // 3 players to act
                                         {0.5f, 1.0f}, // 4 players to act
                                         {0.5f, 1.0f}, // 5 players to act
